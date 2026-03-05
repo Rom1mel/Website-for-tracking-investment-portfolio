8b49bf7f286df8332ab43249503d3b564ccc31b2
NonceHunter <romarak2005@gmail.com>
2026-03-05 16:12:21 +0300

    Заглушка для домашней страницы

--- trackingportfolio/trackingportfolio/urls.py
+++ trackingportfolio/trackingportfolio/urls.py
@@ -24,5 +24,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('home.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
