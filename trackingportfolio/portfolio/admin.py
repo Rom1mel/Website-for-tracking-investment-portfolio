b296104283dc711d90334426a3e42383711a8321
NonceHunter <romarak2005@gmail.com>
2026-03-10 17:02:47 +0300

    База данных
    Создание основных таблиц, настройка связей между ними. Регистрация таблиц в в окне администратора.

--- trackingportfolio/portfolio/admin.py
+++ trackingportfolio/portfolio/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
 
-# Register your models here.
+from . models import *
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'time_stamp']
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ['ticker', 'name', 'count', 'portfolio']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['type', 'portfolio', 'asset', 'value']
--- trackingportfolio/portfolio/models.py
+++ trackingportfolio/portfolio/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='portfolio')
+    time_stamp = models.DateTimeField(auto_now_add=True, editable=False)
+    def __str__(self):
+        return self.name
+
+class Asset(models.Model):
+    ticker = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='asset')
+    count = models.PositiveIntegerField()
+    purchase_price_per_unit = models.PositiveIntegerField()
+    time_stamp = models.DateTimeField(auto_now_add=True, editable=False)
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    type = models.CharField(max_length=50)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='transaction')
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE, related_name='transaction')
+    value = models.PositiveIntegerField()
+    time_stamp = models.DateTimeField(auto_now_add=True, editable=False)
+    def __str__(self):
+        return self.type
