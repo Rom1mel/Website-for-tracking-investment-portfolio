9058b201dc765354504417b881b1d50653ec4771
NonceHunter <romarak2005@gmail.com>
2026-04-03 16:20:00 +0300

    Отображение всех портфелей
    Теперь при выборе все портфели на странице портфелей записи об одном активе не дублируются, даже если один и тот же актив есть в разных портфелях.

--- trackingportfolio/portfolio/views.py
+++ trackingportfolio/portfolio/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import PortfolioAsset, Portfolio
 from .forms import PortfolioForm
+from django.db.models import Sum, F, FloatField, ExpressionWrapper
 
 def portfolio(request):
     portfolio_id = request.GET.get('portfolio_id', 'all')
     if portfolio_id == 'all':
-        assets = PortfolioAsset.objects.filter(portfolio__user=request.user)
+        assets = (PortfolioAsset.objects.
+            filter(portfolio__user=request.user).values('asset__name').
+            annotate(total_count = Sum('count'), total_value =Sum(F('count')*F('price'))).
+            annotate(avg_price=ExpressionWrapper(
+                F('total_value') / F('total_count'),
+                output_field=FloatField())))
     else:
-        assets = PortfolioAsset.objects.filter(portfolio_id=portfolio_id, portfolio__user=request.user)
+        assets = (PortfolioAsset.objects.
+            filter(portfolio_id=portfolio_id, portfolio__user=request.user).values('asset__name').
+            annotate(total_count = Sum('count'), total_value =Sum('price')).
+            annotate(avg_price=ExpressionWrapper(F('total_value') / F('total_count'),
+                 output_field=FloatField())))
     portfolios = Portfolio.objects.filter(user=request.user)
     data = {'assets': assets,
         'portfolios': portfolios,
